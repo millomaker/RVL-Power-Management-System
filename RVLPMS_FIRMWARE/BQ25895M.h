33768c70e4c361823d4cd1152dbdd184501be33f
millomaker <bj.millot24@gmail.com>
2021-10-01 20:44:30 +0200

    BengeAdd

--- RVLPMS_FIRMWARE/BQ25895M.h
+++ RVLPMS_FIRMWARE/BQ25895M.h
@@ -15,11 +15,13 @@
 void BQ_Write(unsigned char reg, char data);
 unsigned short BQ_Read(unsigned char reg);
 void BQ_get_chrg_state();
+void BQ_get_VbusV(); //BengeAdd
 void BQ_CONFIG_INIT();
 void BQ_INIT();
 void BQ_UPDATE();
 
 extern uint8_t VBUS_CHRG_STATE[2];
+extern uint8_t VBUSV_STATE[2]; //BengeAdd
 extern uint8_t BATTERY_VOLTAGE;
 extern uint8_t BQ_adc_state;
 
